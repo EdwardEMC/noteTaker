ea87f503b4c7e2c91c2f5911e969250eff50f4fb
EMC <edwardcoad.ec@gmail.com>
2020-01-23 08:45:32 +1030

    api routing complete, delete now needed

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true })); //not sure if needed
 app.use(express.json());
 
+app.use(express.static("public"));
+
 //normally section for data, but storing that in db.json instead
 
 //setting up routing
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
@@ -22,29 +24,64 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    return res.json(path.join(__dirname, "db.json"));
+    fs.readFile("db.json", "utf8", function(err, data) {
+        if(err) {
+            throw err;
+        }
+        return res.json(JSON.parse(data));
+    });
 });
 
 //post
 app.post("/api/notes", function(req, res) {
     var newNote = req.body;
 
-    newNote.routeName=newNote.title.replace(/\s+/g, "").toLowerCase();
+    newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newNote);
+    console.log(JSON.stringify(newNote, null, 2));
 
-    //save data to db.json
-    fs.appendFile("db.json", newNote, function(err) {
+    fs.readFile("db.json", "utf8", function(err, data) { //reading db.json to see what is already saved
         if(err) {
             throw err;
         }
-        console.log("Saved");
-    });
+        else {
+            obj = JSON.parse(data);
+            obj.push(newNote);
+            json = JSON.stringify(obj, null, 2);
+        }
 
+        fs.writeFile("db.json", json, function(err) {//appending data to db.json
+            if(err) {
+                throw err;
+            }
+            console.log("Saved");
+        });
+    });
     res.json(newNote);
 });
 
-app.delete("/api/tables/:id", function(req, res) {
+app.get("/api/notes/:id", function(req, res) {
+    var note = req.params.id;
+  
+    console.log(note);
+
+    fs.readFile("db.json", "utf8", function(err, data) {
+        if(err) {
+            throw err;
+        }
+        const history = JSON.parse(data);
+
+        for (var x = 0; x < history.length; x++) {
+            if (note === history[x].routeName) {
+                return res.json(history[x]);
+            }
+        }
+
+        return res.json(false);
+    });
+});
+
+app.delete("/api/notes/:id", function(req, res) {
     savedspace.splice(req.params.id, 1); //change savedspace to db.json file
     res.json(savedspace);
 });
